53c68f33ca8dec01f8f7430765db387ffb293a92
ranjan1975 <90087712+ranjan1975@users.noreply.github.com>
2023-02-08 10:54:22 -0500

    Third addition of try_except1.py

---
+++ try_except1.py
@@ -0,0 +1,11 @@
+try:
+    numerator = int(input("Enter a numerator: "))
+    denominator = int(input("Enter a denominator"))
+    result =  numerator/denominator
+    print(result)
+except:
+    print("Denominator can not be 0, please try again")
+
+print("Program ends")
+
+        
